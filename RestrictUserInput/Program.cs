9d511920cf110d8085affc51b977067393b946dd
abt-111 <anthony.temmel@gmail.com>
2024-06-15 13:01:48 +0200

    Ajout de commentaire et de code pour permuter les valeurs si leur ordre pose problème

--- RestrictUserInput/Program.cs
+++ RestrictUserInput/Program.cs
@@ -5,49 +5,68 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        bool isValidInput;
+        bool isValidInput = false;
         int limit1 = 0,
             limit2 = 0,
             number = 0;
 
         do
         {
-            isValidInput = true;
             try
             {
+                // Saisie de l'utilisateur
                 Console.WriteLine("Entrez une première limite: ");
                 limit1 = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Entrez une deuxième limite: ");
                 limit2 = Convert.ToInt32(Console.ReadLine());
 
+                // Si la première limite est supérieure à la deuxième…
+                if (limit1 > limit2)
+                {
+                    // … on permute les valeurs;
+                    int permutationVariable = limit1;
+                    limit1 = limit2;
+                    limit2 = permutationVariable;
+                }
+
                 Console.WriteLine("Veuillez entrer un nombre compris entre " + limit1 + " et " + limit2);
                 number = Convert.ToInt32(Console.ReadLine());
+
+                // Aucune exception n'a été levées
+                isValidInput = true;
             }
             catch (Exception exception)
             {
                 if (exception is FormatException || exception is OverflowException)
                 {
-                    Console.WriteLine($"Vous devez entrer des nombres entiers compris en {Int32.MinValue} et {Int32.MaxValue}");
-                    isValidInput = false;
+                    Console.WriteLine($"Vous devez entrer des nombres entiers compris en {Int32.MinValue} et {Int32.MaxValue} \n");
                 }
             }
         }
         while(!isValidInput);
 
-        if (number <= limit2 && number >= limit1)
+        // Si number est compris dans l'intervalle [limit1;limit2]…
+        if (number >= limit1 && number <= limit2)
         {
+            // … on affiche sa valeur
             Console.WriteLine("Resulting integer: " + number);
         }
-        else if (number > limit2)
+        // Si number est inférieur à limit1…
+        else if (number < limit1)
         {
-            Console.WriteLine("You have entered " + number + " which is greater than " + limit2 + " which is the maximum");
-            Console.WriteLine("Resulting integer: " + limit2);
+            // … on affiche un message le message idoine…
+            Console.WriteLine("You have entered " + number + " which is lower than " + limit1 + " which is the minimum");
+            // … on renvoie la valeur de limite1
+            Console.WriteLine("Resulting integer: " + limit1);
         }
+        // Si number est supérieur à limit2…
         else
         {
-            Console.WriteLine("You have entered " + number + " which is lower than " + limit1 + " which is the minimum");
-            Console.WriteLine("Resulting integer: " + limit1);
+            // … on affiche un message le message idoine…
+            Console.WriteLine("You have entered " + number + " which is greater than " + limit2 + " which is the maximum");
+            // … on renvoie la valeur de limite2
+            Console.WriteLine("Resulting integer: " + limit2);
         }
     }
 }
