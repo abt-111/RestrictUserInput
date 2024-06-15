5391d017242cf846caf3b0e779eb4dc1ce1daeee
abt-111 <anthony.temmel@gmail.com>
2024-06-15 12:35:33 +0200

    Ajout controle des exceptions levées par les entrées invalides de l'utilisateur

--- RestrictUserInput/Program.cs
+++ RestrictUserInput/Program.cs
@@ -1,22 +1,45 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Limit1: ");
-        int limit1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Limit2: ");
-        int limit2 = Convert.ToInt32(Console.ReadLine());
+        bool isValidInput;
+        int limit1 = 0,
+            limit2 = 0,
+            number = 0;
 
-        Console.WriteLine("Veuillez entrer un nombre compris entre " + limit1 + " et " + limit2);
-        int number = Convert.ToInt32(Console.ReadLine());
+        do
+        {
+            isValidInput = true;
+            try
+            {
+                Console.WriteLine("Entrez une première limite: ");
+                limit1 = Convert.ToInt32(Console.ReadLine());
 
-        if(number <= limit2 && number >= limit1)
+                Console.WriteLine("Entrez une deuxième limite: ");
+                limit2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Veuillez entrer un nombre compris entre " + limit1 + " et " + limit2);
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception exception)
+            {
+                if (exception is FormatException || exception is OverflowException)
+                {
+                    Console.WriteLine($"Vous devez entrer des nombres entiers compris en {Int32.MinValue} et {Int32.MaxValue}");
+                    isValidInput = false;
+                }
+            }
+        }
+        while(!isValidInput);
+
+        if (number <= limit2 && number >= limit1)
         {
             Console.WriteLine("Resulting integer: " + number);
         }
-        else if(number > limit2)
+        else if (number > limit2)
         {
             Console.WriteLine("You have entered " + number + " which is greater than " + limit2 + " which is the maximum");
             Console.WriteLine("Resulting integer: " + limit2);
